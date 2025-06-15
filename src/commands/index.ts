19f8adf1577cde843c28324df5a92003ca6d7b14
Anirudh Sriram <hello@techwithanirudh.com>
2025-06-15 08:45:08 +0000

    Add voice channel command structure: introduce 'vc' command with join and leave subcommands, and update package.json for deployment script

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,9 @@
 import * as ping from './ping';
 import * as channels from './channels';
-import * as join from './voice-channel/join';
-import * as leave from './voice-channel/leave';
+import * as vc from './voice-channel';
 
 export const commands = {
   ping,
   channels,
-  join,
-  leave,
+  vc,
 };
